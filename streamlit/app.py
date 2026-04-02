e44417e039c3687d68543641f0e1d2d6ac89ba05
Thib0 <124298493+Thib0727@users.noreply.github.com>
2026-04-02 14:45:41 +0200

    Update app.py
    ajout des pages

--- streamlit/app.py
+++ streamlit/app.py
@@ -6,6 +6,9 @@ from vues.page_admin import gestion_user
 from fonctions.connexion_sql import get_connection_db_tdb, connexion_mysql_root, base_existe
 from fonctions.requetes_sql import *
 import pandas as pd
+from vues.visu_Production import dashboard_visu_Production
+from vues.visu_Qualité_Logistique import dashboard_visu_Qualité_Logistique
+from vues.visu_Gestion_des_Stocks_Robotique import dashboard_visu_Gestion_des_Stocks_Robotique
 
 # Pour rendre le login obligatoire
 if not check_auth(): 
@@ -15,8 +18,58 @@ if not check_auth():
 # --- Sidebar ---
 st.sidebar.title("Navigation")
 
+# --- Dans app.py ---
+st.markdown("""
+    <style>
+    /* Fond de l'application */
+    .stApp { background-color: #2d2942; }
+    
+    /* Titre st.title en blanc pur */
+    .stApp h1 { 
+        color: white !important; 
+        font-weight: bold !important;
+        text-shadow: 1px 1px 2px rgba(0,0,0,0.5);
+    }
+
+    /* Style des cartes KPI */
+    [data-testid="stMetric"] {
+        background-color: #413d58;
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #5d587a;
+    }
+
+    /* Labels des KPI (Gris clair mais lisible) */
+    [data-testid="stMetricLabel"] > div {
+        color: #e0e0e0 !important;
+    }
+    
+    /* Valeurs des KPI (Blanc pur) */
+    [data-testid="stMetricValue"] > div {
+        color: white !important;
+        font-weight: bold !important;
+    }
+
+    /* Correction du texte général : On enlève l'opacité 0.9 qui grisait tout */
+    .stMarkdown div p {
+        color: white !important;
+    }
+    .block-container {
+    padding-top: 3rem !important;
+    padding-bottom: 1rem !important;
+    padding-left: 2rem !important;
+    padding-right: 2rem !important;
+    max-width: 98% !important; /* Force l'utilisation de la quasi-totalité de la largeur */
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+
+
 # Liste des pages accessibles selon le rôle
-pages = ["Accueil", "Importer la base de données"]
+pages = ["Accueil", "Importer la base de données", "Production", "Qualité & Logistique", "Gestion des Stocks & Robotique"]
+
 
 # Ajouter la page admin si rôle = "admin"
 if st.session_state.role == "admin":
@@ -83,3 +136,10 @@ elif st.session_state.page_active == "Importer la base de données":
     import_bdd()
 elif st.session_state.page_active == "Gestion des utilisateurs":
     gestion_user()
+elif st.session_state.page_active == "Production":
+    dashboard_visu_Production()
+elif st.session_state.page_active == "Qualité & Logistique":
+    dashboard_visu_Qualité_Logistique()
+elif st.session_state.page_active == "Gestion des Stocks & Robotique":
+    dashboard_visu_Gestion_des_Stocks_Robotique()
+
